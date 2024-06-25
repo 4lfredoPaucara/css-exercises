eceeb5204f4438f388fa286a94a48747de673b3b
Alfredo Paucara <4lfredo.paucara@gmail.com>
2024-06-24 20:48:01 -0400

    realizados 5 ejercicios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Exercise1Component } from './exercise1/exercise1.component';
+import { Exercise2Component } from './exercise2/exercise2.component';
+import { Exercise3Component } from './exercise3/exercise3.component';
+import { Exercise5Component } from './exercise5/exercise5.component';
+import { Exercise4Component } from './exercise4/exercise4.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Exercise1Component,
+    Exercise2Component,
+    Exercise3Component,
+    Exercise5Component,
+    Exercise4Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/exercise4/exercise4.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exercise4',
+  templateUrl: './exercise4.component.html',
+  styleUrls: ['./exercise4.component.scss']
+})
+export class Exercise4Component {
+
+}
